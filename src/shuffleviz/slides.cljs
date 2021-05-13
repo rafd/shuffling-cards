3383ba53ccc5527f60c910e5699cc2b3414f3c07
Rafal Dittwald <rafal.dittwald@gmail.com>
2021-05-12 21:53:47 -0400

    Rework presentation content; add diagrams; allow sequences

--- src/shuffleviz/slides.cljs
+++ src/shuffleviz/slides.cljs
@@ -19,10 +19,11 @@
 
    {:name "Overhand Shuffle (aka Hindu Shuffle)"
     :f shuffles/overhand-shuffle
-    :image "overhand.png"}
+    :image "diagram-overhand.png"}
 
-   {:name "Middle Pull Shuffle"
-    :f shuffles/middle-pull-alternating-shuffle}
+   #_{:name "Middle Pull Shuffle"
+    :f shuffles/middle-pull-alternating-shuffle
+    :image "diagram-pull.png"}
 
    #_{:name "Fisher-Yates Shuffle (Clojure, Java, etc.)"
     :f shuffle}
@@ -34,13 +35,13 @@
     :f shuffles/biased-pick-one-at-a-time-shuffle
     :repeat-pointless? true}
 
-   {:name "Milk Shuffle"
+   #_{:name "Milk Shuffle"
     :f shuffles/milk-shuffle
     :perfect? true
-    :image "milk.png"}
+    :image "diagram-milk.png"}
    {:name "Sloppy Milk Shuffle"
+    :image "diagram-milk.png"
     :f shuffles/sloppy-milk-shuffle}
-
 
 
    #_{:name "Raf's Shuffle"
@@ -52,30 +53,24 @@
    {:name "Perfect Riffle Shuffle (aka Dovetail, Faro)"
     :f shuffles/perfect-interleave-shuffle
     :perfect? true
-    :image "perfect-riffle.png"}
+    :image "diagram-riffle.png"}
    {:name "Near-Perfect Riffle Shuffle"
     :f shuffles/interleave-shuffle
-    :image "riffle.png"}
-   {:name "Near-Perfect Riffle Shuffle (64 cards)"
+    :image "diagram-riffle.png"}
+   #_{:name "Near-Perfect Riffle Shuffle (64 cards)"
     :f shuffles/interleave-shuffle
+    :image "diagram-riffle.png"
     :card-count 64}
    {:name "Sloppy Riffle Shuffle"
+    :image "diagram-riffle.png"
     :f shuffles/sloppy-interleave-shuffle}
+   {:name "Overhand + Riffle + Milk"
+    :f [shuffles/overhand-shuffle
+        shuffles/sloppy-milk-shuffle
+        shuffles/sloppy-interleave-shuffle]}
    #_{:name "Sloppy Riffle Shuffle (24 cards)"
     :f (comp shuffles/sloppy-interleave-shuffle shuffles/middle-pull-alternating-shuffle)
     :card-count 24}])
-
-#_(defn stateful-table-view
-  [_]
-  (let [shuffles (r/atom [])]
-    (fn [{:keys [in repeat-times reapply-times shuffle-fn]}]
-      [:section {:on-click
-                 (fn []
-                   (swap! shuffles concat (repeatedly repeat-times #(shuffles/reapply shuffle-fn reapply-times in))))}
-       [:div {:style {:text-align "center"
-                      :font-weight "bold"}}
-        reapply-times "x"]
-       [matrix/table in @shuffles]])))
 
 (defn slide-view
   [_]
@@ -85,28 +80,38 @@
             columns (if repeat-pointless?
                       (range 1)
                       (range 8))
+            fs (if (fn? f)
+                 (take 8 (cycle [f]))
+                 (take 8 (cycle f)))
+            single-compute (if (fn? f)
+                             (fn [f-count]
+                               (shuffles/reapply f f-count in-cards))
+                             (fn [f-count]
+                               (reduce calc/transform (map (fn [f] (f in-cards)) (take f-count fs)))))
+            compute-monte-carlo! (fn big-calc [column]
+                                   (swap! data update-in [:repeats column] concat
+                                          (repeatedly 1000 #(single-compute (inc column))))
+                                   (when (< column (dec (count columns)))
+                                     (js/requestAnimationFrame #(big-calc (inc column)))))
             compute! (fn []
-                       (let [singles (mapv (fn [_]
+                       (let [singles (mapv (fn [f]
                                              (f in-cards))
-                                           columns)
-                             reapplies (vec (reductions calc/transform singles))
-                             big-calc (fn big-calc [column]
-                                        (swap! data update-in [:repeats column] concat
-                                               (repeatedly 1000 #(shuffles/reapply f (inc column) in-cards)))
-                                        (when (< column (dec (count columns)))
-                                          (js/requestAnimationFrame #(big-calc (inc column)))))]
+                                           fs)
+                             reapplies (vec (reductions calc/transform singles))]
                          (swap! data assoc
                                 :singles singles
                                 :reapplies reapplies)
-                         (js/requestAnimationFrame #(big-calc 0))))]
+                         #_(js/requestAnimationFrame #(compute-monte-carlo! 0))))]
         [:div {:style {:margin-top "10em"}}
-         #_(when image
-           [:div {:style {:text-align "center"}}
-            [:img {:src image}]])
          [:h1 {:style {:font-size "3.5em"
                        :text-align "center"}}
-          name
-          [:button {:on-click (fn [] (compute!))} "Compute"]]
+          [:img {:src image
+                 :style {:vertical-align "middle"
+                         :margin-right "0.5em"
+                         :height "2em"}}]
+          [:span {:style {:vertical-align "middle"}} name]
+          [:button {:on-click (fn [] (compute!))} "Compute"]
+          [:button {:on-click (fn [] (compute-monte-carlo! 0))} "Monte Carlo"]]
          ;; 1x
          [:div {:style {:display "flex"
                         :justify-content "space-between"}}
@@ -145,7 +150,8 @@
             (for [column columns]
               ^{:key column}
               [:div {:on-click (fn []
-                                 (swap! data update-in [:repeats column] concat (repeatedly 1000 #(shuffles/reapply f (inc column) in-cards))))}
+                                 (swap! data update-in [:repeats column] concat
+                                        (repeatedly 1000 #(single-compute (inc column)))))}
 
                [matrix/table in-cards (get-in @data [:repeats column])]
                [:div {:style {:text-align "center"
@@ -170,17 +176,14 @@
        [:h1 {:style {:text-align "center"}} "identity"]
        [stateful-table-view in-cards 1 identity]]
       [:div
-       [:h1 {:style {:text-align "center"}} "reverse"]
-       [stateful-table-view in-cards 1 reverse]]
-      [:div
-       [:h1 {:style {:text-align "center"}} "cut"]
-       [stateful-table-view in-cards 1 shuffles/pure-rand-cut]]
-      [:div
-       [:h1 {:style {:text-align "center"}} "riffle"]
-       [stateful-table-view in-cards 1 shuffles/perfect-interleave-shuffle]]
-      [:div
        [:h1 {:style {:text-align "center"}} "overhand"]
        [stateful-table-view in-cards 1 shuffles/overhand-shuffle]]
+      [:div
+       [:h1 {:style {:text-align "center"}} "milk"]
+       [stateful-table-view in-cards 1 shuffles/sloppy-milk-shuffle]]
+      [:div
+       [:h1 {:style {:text-align "center"}} "riffle"]
+       [stateful-table-view in-cards 1 shuffles/sloppy-interleave-shuffle]]
       [:div
        [:h1 {:style {:text-align "center"}} "fisher-yates"]
        [stateful-table-view in-cards 1 shuffle]]]
@@ -192,16 +195,13 @@
        [stateful-table-view in-cards 1000 identity]]
       [:div
        [:h1 {:style {:text-align "center"}} "x1000"]
-       [stateful-table-view in-cards 1000 reverse]]
-      [:div
-       [:h1 {:style {:text-align "center"}} "x1000"]
-       [stateful-table-view in-cards 1000 shuffles/pure-rand-cut]]
-      [:div
-       [:h1 {:style {:text-align "center"}} "x1000"]
-       [stateful-table-view in-cards 1000 shuffles/perfect-interleave-shuffle]]
-      [:div
-       [:h1 {:style {:text-align "center"}} "x1000"]
        [stateful-table-view in-cards 1000 shuffles/overhand-shuffle]]
+      [:div
+       [:h1 {:style {:text-align "center"}} "x1000"]
+       [stateful-table-view in-cards 1000 shuffles/sloppy-milk-shuffle]]
+      [:div
+       [:h1 {:style {:text-align "center"}} "x1000"]
+       [stateful-table-view in-cards 1000 shuffles/sloppy-interleave-shuffle]]
       [:div
        [:h1 {:style {:text-align "center"}} "x1000"]
        [stateful-table-view in-cards 1000 shuffle]]]
@@ -212,6 +212,7 @@
                     :margin-top "2em"
                     :text-align "center"}}
       [:img {:src "/riffle.png" :style {:vertical-align "top"} :width "30%"}]
+      [:img {:src "/milk.png" :style {:vertical-align "top"} :width "40%"}]
       [:img {:src "/overhand.png" :width "30%"}]]]))
 
 (defn slides-view []
@@ -224,6 +225,9 @@
    [:div {:style {:margin-top "20em"
                   :padding-bottom "10em"
                   :display "flex"
+                  :flex-direction "column"
+                  :align-items "center"
                   :justify-content "center"}}
+    [:h1 "???"]
     [stateful-table-view (range 52) 100 (fn [in]
                                            (shuffles/reapply shuffles/raf-shuffle (rand-int (/ 52 2)) in))]]])
