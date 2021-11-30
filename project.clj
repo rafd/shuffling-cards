d5bb3a06e42f001fa4f1118e8c6ea1ff37121ee2
Rafal Dittwald <rafal.dittwald@gmail.com>
2021-11-30 17:31:27 -0500

    Attempt fix dependency issue

--- project.clj
+++ project.clj
@@ -5,7 +5,11 @@
                  [io.bloomventures/commons "0.11.2"]
                  [cljsjs/d3 "6.2.0-0"]
                  [garden "1.3.9"]
-                 [reagent "0.10.0"]]
+                 [reagent "0.10.0"]
+
+                 ;; dep fixes:
+                 [com.fasterxml.jackson.core/jackson-core "2.11.2"]
+                 [com.fasterxml.jackson.core/jackson-databind "2.11.2"]]
 
   :main shuffleviz.core
 
