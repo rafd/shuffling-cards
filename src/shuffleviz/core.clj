31333b9fbc10596bf0de7ecab2b17e6b843ed5ba
Rafal Dittwald <rafal.dittwald@gmail.com>
2021-11-30 18:25:52 -0500

    Read http-port from file

--- src/shuffleviz/core.clj
+++ src/shuffleviz/core.clj
@@ -1,11 +1,15 @@
 (ns shuffleviz.core
   (:gen-class)
   (:require
+    [bloom.commons.config :as config]
     [bloom.omni.core :as omni]))
 
 (def config
   {:omni/title "Card Shuffling Visualization"
-   :omni/cljs {:main "shuffleviz.core"}})
+   :omni/cljs {:main "shuffleviz.core"}
+   :omni/http-port (config/read "config.edn"
+                                [:map
+                                 [:http-port integer?]])})
 
 (defn start! []
   (omni/start! omni/system config))
