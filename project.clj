d4d4a008a7d1da29cd11976bfcd43e6649def61b
Rafal Dittwald <rafal.dittwald@gmail.com>
2021-12-06 23:13:03 -0500

    Add emoji-favicon

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/clojurescript "1.10.764"]
                  [io.bloomventures/omni "0.27.7"]
-                 [io.bloomventures/commons "0.11.2"]
+                 [io.bloomventures/commons "0.11.4"]
                  [cljsjs/d3 "6.2.0-0"]
                  [garden "1.3.9"]
                  [reagent "0.10.0"]
--- src/shuffleviz/ui.cljs
+++ src/shuffleviz/ui.cljs
@@ -1,8 +1,10 @@
 (ns shuffleviz.ui
   (:require
+    [bloom.commons.ui.emoji-favicon :refer [emoji-favicon]]
     [shuffleviz.explore :as explore]
     [shuffleviz.slides :as slides]))
 
 (defn app-view []
   [:div
+   [emoji-favicon "🃏"]
    [slides/slides-view]])
